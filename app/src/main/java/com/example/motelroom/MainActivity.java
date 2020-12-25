b855301d33464c9af3b3efd5261219f8200484a2
hoanghuuf17 <hoanghuuf17@gmail.com>
2020-12-25 13:43:35 +0700

    change dafault

--- app/src/main/java/com/example/motelroom/MainActivity.java
+++ app/src/main/java/com/example/motelroom/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.motelroom;
 
+
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
-import com.example.motelroom.categories.ChungCuFragment;
-import com.example.motelroom.categories.NhaNguyenCanFragment;
-import com.example.motelroom.categories.OGhepFragment;
-import com.example.motelroom.categories.PhongTroFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
-import androidx.fragment.app.FragmentManager;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -25,6 +24,7 @@ import androidx.appcompat.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,9 +47,26 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.content_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                int menuId = destination.getId();
+                switch (menuId){
+                    case R.id.nav_gallery:
+                        Toast.makeText(MainActivity.this, "you tapped gallery", Toast.LENGTH_LONG).show();
+                        fab.hide();
+                        break;
+                    default:
+                        fab.show();
+                        break;
+                }
+            }
+        });
+
     }
 
     @Override
@@ -61,39 +78,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.content_main);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
-
-//    //thieu Override
-//    public boolean onNavigationItemsSelected(MenuItem item) {
-//        int id = item.getItemId();
-//
-//        if (id == R.id.home) {
-//
-//        } else if (id == R.id.phongtro) {
-//            PhongTroFragment phongTroFragment = new PhongTroFragment();
-//            FragmentManager manager = getSupportFragmentManager();
-//            manager.beginTransaction().replace(R.id.content_main, phongTroFragment, phongTroFragment.getTag()).commit();
-//
-//
-//        } else if (id == R.id.chuncu) {
-//            ChungCuFragment chungCuFragment = new ChungCuFragment();
-//            FragmentManager manager = getSupportFragmentManager();
-//            manager.beginTransaction().replace(R.id.content_main, chungCuFragment, chungCuFragment.getTag()).commit();
-//
-//
-//        } else if (id == R.id.oghep) {
-//            OGhepFragment oGhepFragment = new OGhepFragment();
-//            FragmentManager manager = getSupportFragmentManager();
-//            manager.beginTransaction().replace(R.id.content_main, oGhepFragment, oGhepFragment.getTag()).commit();
-//
-//        } else if (id == R.id.nhanguyencan) {
-//            NhaNguyenCanFragment nhaNguyenCanFragment = new NhaNguyenCanFragment();
-//            FragmentManager manager = getSupportFragmentManager();
-//            manager.beginTransaction().replace(R.id.content_main, nhaNguyenCanFragment, nhaNguyenCanFragment.getTag()).commit();
-//
-//        }
-//    }
 }
